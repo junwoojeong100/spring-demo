4dee285953ed833881ae34ceca94c212c9f9ac76
junw9293 <joonoo.jeong@gmail.com>
2022-08-19 09:38:22 +0900

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
