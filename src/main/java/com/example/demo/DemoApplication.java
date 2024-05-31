e28d2336747ede35cd62b0f7028f68df259483d0
Junwoo Jeong <junwoojeong100@gmail.com>
2024-05-31 11:35:26 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 	@RestController
 	public class DemoApplication {
 
-
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 	@GetMapping("/")
 	//@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		System.out.println("'/hello' called");
+		LocalDateTime now = LocalDateTime.now();
+        	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		System.out.println(now.format(formatter)) + " '/hello' called.");
 		return String.format("Hello %s!", name);
 	}
 
